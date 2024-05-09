0f1c1ed35af33a2b3e6c4326dc28ee5d16e172c6
Pengcheng Ma <ma169@uwindsor.ca>
2024-05-09 16:27:59 +0800

    无

---
+++ sky-common/src/main/java/com/sky/exception/UsernameExistException.java
@@ -0,0 +1,12 @@
+package com.sky.exception;
+
+public class UsernameExistException extends BaseException{
+
+    public UsernameExistException(){}
+
+    public UsernameExistException(String message) {
+        super(message);
+    }
+
+
+}
